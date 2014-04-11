81176bcd7d438973b21f3123a1b933f7cf37732a
Nick Kontos <nicholas.kontos@stonybrook.edu>
2014-04-11 17:28:16 -0400

    Added links

--- AstroJump/AstroJump/stdafx.h
+++ AstroJump/AstroJump/stdafx.h
@@ -10,7 +10,11 @@
 #include <stdio.h>
 #include <tchar.h>
 
-
+#include "psti_VS\stdafx.h"
+#include "sssf_VS\stdafx.h"
+#include "tinyxml_VS\stdafx.h"
+#include "tmxmi_VS\stdafx.h"
+#include "xmlfi_VS\stdafx.h"
 
 
 // TODO: reference additional headers your program requires here
